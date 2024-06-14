622532e168813e9c2563c55523479b2ca8f35b34
Sagar-Karmoker <sagarkarmoker2001@gmail.com>
2024-06-14 13:35:21 +0600

    project setup [done] ✅

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import {
+  ThirdwebProvider,
+  ConnectButton 
+} from "thirdweb/react";
+import { inAppWallet } from "thirdweb/wallets";
+import { createThirdwebClient } from "thirdweb";
+
+// we need to use env variables to store the client id
+const client = createThirdwebClient({
+  clientId: "ee6dfcd5ba3897d57f213689f67f19ab"
+});
+
+const wallets = [
+  inAppWallet({
+    auth: {
+      options: ["email", "phone", "google"],
+    },
+  }),
+];
+
+export default function App() {
+  return (
+    <ThirdwebProvider>
+      <ConnectButton
+        client={client}
+        wallets={wallets}
+        theme={"dark"}
+        connectModal={{
+          size: "wide",
+          showThirdwebBranding: false,
+        }}
+      />
+    </ThirdwebProvider>
+  );
+}
