2643034879e32aeb156b128363b27cc4e4610a45
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-07-28 02:15:47 +0600

    access to dist dash [done]

--- src/components/distributors/DistributorDashboard.jsx
+++ src/components/distributors/DistributorDashboard.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
-import { useActiveAccount } from 'thirdweb/react'
+import { useState, useEffect } from 'react'
+import { useActiveAccount, useActiveWalletConnectionStatus } from 'thirdweb/react';
+import { isDistributor } from '../utils/RoleCheck';
 
 function DistributorDashboard({ setActiveComponent }) {
     const activeAccount = useActiveAccount();
+    const [checkDistributor, setCheckDistributor] = useState(false);
+    const status = useActiveWalletConnectionStatus();
 
-    if (activeAccount?.address == '') {
-        return <>
+    useEffect(() => {
+        const checkIfDistributor = async () => {
+            if (activeAccount?.address) {
+                const result = await isDistributor(activeAccount.address);
+                console.log(result);
+                setCheckDistributor(result);
+            }
+        };
+
+        checkIfDistributor();
+    }, [activeAccount]);
+
+    if (status === 'disconnected') {
+        return (
             <div className='flex flex-col justify-center items-center h-[90vh]'>
                 <h1 className='text-3xl font-bold text-red-500'>Access Denied</h1>
                 <br />
@@ -14,7 +29,21 @@ function DistributorDashboard({ setActiveComponent }) {
                     Go back
                 </button>
             </div>
-        </>
+        );
+    }
+
+    if (status === 'connected' && !checkDistributor) {
+        return (
+            <div className='flex flex-col justify-center items-center h-[90vh]'>
+                <h1 className='text-3xl font-bold text-red-500'>Access Denied</h1>
+                <br />
+                <p className='text-red-400'>You are not an distributor</p>
+                <p className='text-red-400'>Please apply for registation</p>
+                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'>
+                    Go back
+                </button>
+            </div>
+        );
     }
 
     return (
