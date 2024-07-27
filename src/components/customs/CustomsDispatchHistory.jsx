dff4058d920caf351d3e98fac6835057f84adbf7
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-07-27 20:26:42 +0600

    customs dispatch history [done]

--- src/components/customs/CustomsDispatchHistory.jsx
+++ src/components/customs/CustomsDispatchHistory.jsx
@@ -1,16 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer, Box, Heading } from '@chakra-ui/react';
+import { useActiveAccount } from 'thirdweb/react';
+import { etherContract } from '../../contants';
 
 function CustomsDispatchHistory() {
+  const [dispatches, setDispatches] = useState([]);
+  const activeAccount = useActiveAccount();
+
+  useEffect(() => {
+    const fetchHistoryData = async () => {
+      const events = await etherContract.queryFilter('ProductDispatched');
+      // Process events
+      const dispatchesList = events.map(event => {
+        const { id, productId, ipfsDocHash, from, to, timestamp, quantity } = event.args;
+        return {
+          id: id.toString(),
+          productId: productId.toString(),
+          ipfsDocHash,
+          from: from,
+          to: to,
+          timestamp: timestamp.toNumber(),
+          quantity: quantity.toString()
+        };
+      });
+
+      setDispatches(dispatchesList);
+    };
+
+    fetchHistoryData();
+  }, []);
+
   return (
-    <>
-      <div>
-        <h1 className='text-center text-4xl font-bold mb-5'>Customs to Importer Dispatch History</h1>
-        <div>
-          show table as dispatch history 
-        </div>
-      </div>
-    </>
-  )
+    <Box p={5}>
+      <Heading as='h1' size='xl' mb={5} textAlign='center'>
+        Customs to Importer Dispatch History
+      </Heading>
+      <TableContainer>
+        <Table variant='simple'>
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Product ID</Th>
+              <Th>IPFS Doc Hash</Th>
+              <Th>From</Th>
+              <Th>To</Th>
+              <Th>Timestamp</Th>
+              <Th>Quantity</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {dispatches
+            .filter(dispatch => dispatch.quantity > 0 && dispatch.from === activeAccount?.address)
+            .map(dispatch => (
+              <Tr key={dispatch.id}>
+                <Td>{dispatch.id}</Td>
+                <Td>{dispatch.productId}</Td>
+                <Td>{dispatch.ipfsDocHash}</Td>
+                {/* {dispatch.from} */}
+                <Td>Self</Td> 
+                <Td>{dispatch.to}</Td>
+                <Td>{new Date(dispatch.timestamp * 1000).toLocaleString()}</Td>
+                <Td>{dispatch.quantity}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
 }
 
-export default CustomsDispatchHistory
+export default CustomsDispatchHistory;
