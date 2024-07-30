bda7f209332215baa89c04e9211ea76b7270e7dd
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-07-30 13:23:56 +0600

    dashboard routing fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import Roles from "../pages/Roles";
 import Error404 from "../pages/Error404";
@@ -9,13 +9,14 @@ import { adminAddr } from "../contants";
 import CustomsPanel from "../pages/customs/CustomsPanel";
 import DistributorPanel from "../pages/distributor/DistributorPanel";
 import RetailerPanel from "../pages/retailer/RetailerPanel";
-import Importer from "../pages/importer/Importer";
 import RegApplication from "../pages/RegApplication";
 import ImporterPanel from "../pages/importer/ImporterPanel";
 import { getUserEmail } from "thirdweb/wallets/in-app";
 import { client } from "../contants";
+import { isAdmin, isCustoms, isImporter, isDistributor, isRetailer } from "./utils/RoleCheck";
 
-function Navbar({ visible }) {
+function Navbar() {
+  const [role, setRole] = useState("");
   const activeAccount = useActiveAccount();
   console.log("Account: ", activeAccount?.address);
 
@@ -26,54 +27,59 @@ function Navbar({ visible }) {
 
   getEmail();
 
+  const roleCheck = async () => {
+    if (activeAccount?.address) {
+      if (isAdmin(activeAccount?.address)) {
+        setRole("admin");
+      }
+      else if (isCustoms(activeAccount?.address)) {
+        setRole("customs");
+      }
+      else if (isImporter(activeAccount?.address)) {
+        setRole("importer");
+      }
+      else if (isDistributor(activeAccount?.address)) {
+        setRole("distributor");
+      }
+      else if (isRetailer(activeAccount?.address)) {
+        setRole("retailer");
+      } else {
+        setRole("");
+      }
+    }
+  }
+
+  useEffect(() => {
+    roleCheck();
+  }, [role])
+
+  console.log(activeAccount?.address)
+  console.log(role)
+
   return (
     <BrowserRouter>
       <nav className="bg-green-400">
         <div className="container mx-auto flex justify-between items-center p-2">
           <h1 className="text-2xl font-bold text-red-500">
-            <Link to='/'><img src="logo.png"></img></Link>
+            <Link to='/dashboard'><img src="logo.png"></img></Link>
           </h1>
           <div>
             <ul className="flex items-baseline gap-x-4 font-semibold">
               {
-                activeAccount?.address && <>
+                role !== "" ? (
                   <li>
                     <Link to="/dashboard" className="text-black font-semibold">
                       Dashboard
                     </Link>
                   </li>
+                ) : (
                   <li>
                     <Link to="/apply" className="text-black font-semibold">
                       Apply for Registration
                     </Link>
                   </li>
-                </>
+                )
               }
-              {/* <li>
-                <Link to="/admin" className="text-black font-semibold">
-                  Retailer
-                </Link>
-              </li>
-              <li>
-                <Link to="/customs" className="text-black font-semibold">
-                  Retailer
-                </Link>
-              </li>
-              <li>
-                <Link to="/importer" className="text-black font-semibold">
-                  Importar
-                </Link>
-              </li>
-              <li>
-                <Link to="/distributor" className="text-black font-semibold">
-                  Distributor
-                </Link>
-              </li>
-              <li>
-                <Link to="/retailer" className="text-black font-semibold">
-                  Retailer
-                </Link>
-              </li> */}
               <li>
                 <AvatarButton />
               </li>
@@ -87,11 +93,6 @@ function Navbar({ visible }) {
         {activeAccount?.address === adminAddr &&
           (<Route path="/admin" element={<AdminPanel />} />)
         }
-
-        {/* <Route path="/importer" element={<Importer />} />
-        <Route path="/distributor" element={<Distributor />} />
-        <Route path="/importerdashboard" element={<ImporterDashboard />} />
-        <Route path="/Importerproductchain " element={<ImporterProductChain />} /> */}
 
         {/* admin routes */}
         <Route path="/admin" element={<AdminPanel />} />
@@ -113,6 +114,33 @@ function Navbar({ visible }) {
         <Route path="/apply" element={<RegApplication />} />
         <Route path="/*" element={<Error404 />} />
         <Route path="*" element={<Error404 />} />
+
+        {/* dashboard */}
+        {
+          role === "admin" && (
+            <Route path="/dashboard" element={<AdminPanel />} />
+          )
+        }
+        {
+          role === "customs" && (
+            <Route path="/dashboard" element={<CustomsPanel />} />
+          )
+        }
+        {
+          role === "importer" && (
+            <Route path="/dashboard" element={<ImporterPanel />} />
+          )
+        }
+        {
+          role === "distributor" && (
+            <Route path="/dashboard" element={<DistributorPanel />} />
+          )
+        }
+        {
+          role === "retailer" && (
+            <Route path="/dashboard" element={<RetailerPanel />} />
+          )
+        }
       </Routes>
     </BrowserRouter>
   );
